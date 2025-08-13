941825421c9bbe0b2ee68ac93ef5471265546c14
coding-tsk <thisunk.23078@obhs.school.nz>
2025-08-14 09:51:09 +1200

    made quiz.py work

--- quiz.py
+++ quiz.py
@@ -6,17 +6,19 @@
 
 '''
 #--------------functions---------------------
+#Make sure the user types a one word alphabetic answer
 def force_str(question):
     ans = input(question)
-    while not ans.isalpha():
-        print("Answer needs to only have letters")
+    while not(ans.lower().strip().isalpha()) or ' ' in ans.strip():
+        print("Answer needs to be one word that only has letters.")
         ans = input(question)
     return ans
 
+#Make sure the user types an int
 def force_int(question):
     ans = input(question)
-    while not ans.isnumeric():
-        print("Answer needs to be a number.")
+    while not ans.isdigit():
+        print("Answer needs to be an integer.")
         ans = input(question)
     return ans
 
@@ -25,21 +27,31 @@ questions = [
     "How many letters are in the english alphabet, including upper and lower case letters? ",
     "What is the longest river in New Zealand? ",
     "What is the longest river in the world?",
-    "What is the largest planet in the solar system? "
+    "What is the largest planet in the solar system? ",
     "What school do you go to? "
     ]
 
 answers = [
     "26",
-    "Waikato River",
+    "Waikato",
     "Nile",
     "Jupiter",
-    "Otago Boys High School"
+    "OBHS"
     ]
 
-q_type = [force_int,force_str,force_str,force_str,force_str]
+#Determines if the answer should be a word or a number
+q_type = [force_int,force_str,force_str,force_str,force_str] 
 
-for q in range(5):
+print("Hello! This is a quiz.\nI will ask you 5 questions. Try to answer them correctly.") #greeting
+
+#Quiz loop
+score = 0
+for q in range(len(questions)):
     ans = q_type[q](questions[q])
     if ans == answers[q]:
-        print("The answer is correct.")
+        print("Your answer is correct.")
+        score += 1
+    else:
+        print("Your answer is incorrect.")
+
+print(f"The quiz is over. You got {score} out of {len(questions)} questions right.")
