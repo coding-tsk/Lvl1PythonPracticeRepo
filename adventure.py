61c241de04c088285441cf260b042bd908885a8b
coding-tsk <thisunk.23078@obhs.school.nz>
2025-08-15 10:03:20 +1200

    Program layout

---
+++ adventure.py
@@ -0,0 +1,13 @@
+'''
+    author: name
+    date: 15/08/2025
+    version: 1.0.0
+    description: Python script to go on an adventure :)
+'''
+#---------------------libraries---------------------------
+
+#---------------------functions---------------------------
+
+#---------------------main routine------------------------
+if __name__ == "__main__":
+    pass
