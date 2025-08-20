48029db524228129b8832719549c377b809c01b1
coding-tsk <thisunk.23078@obhs.school.nz>
2025-08-21 09:04:48 +1200

    Made rounding.py and added float fuctionality

---
+++ rounding.py
@@ -0,0 +1,16 @@
+'''
+    author: ME
+    date: 21/08/2025
+    version: 1.0.0
+    description: Differnt ways to round stuff.
+'''
+
+#---------------main routine----------------------
+
+
+total = (10.2823 +7.6991)/2.66
+'''Division & addition with some floats'''
+
+print(f"This is the total {total}",total) #Two ways to print out num alongside strings
+
+#
